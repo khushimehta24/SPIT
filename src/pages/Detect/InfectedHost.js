ab4bdd54c99c3664d80fb89aa818930fe0054ed2
harsh0989 <harshmangukiya1506@gmail.com>
2023-02-05 04:59:45 +0530

    infectedhost.js created

---
+++ src/pages/Detect/InfectedHost.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import UploadImg from '../../sections/@dashboard/barcode/UploadImg'
+
+function InfectedHost() {
+    return (
+        <>
+            <h3>Infected Host</h3>
+            <p style={{ fontSize: '12px' }}>nfected hosts on a network refer to devices that have been compromised by malware or some other type of security threat. These devices can be infected with a variety of malicious software, including viruses, worms, trojans, or other types of malware.Once a host is infected, the attacker can use it to carry out malicious activities, such as stealing sensitive information, spreading malware to other devices on the network, or launching attacks on other systems</p>
+            <UploadImg />
+
+
+        </>
+    )
+}
+
+export default InfectedHost
