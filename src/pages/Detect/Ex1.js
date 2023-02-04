99dde87787a85fe5a6b816fcb7e5ec54e3c4da8b
harsh0989 <harshmangukiya1506@gmail.com>
2023-02-05 04:58:15 +0530

    Ex1.js modified

--- src/pages/Detect/Ex1.js
+++ src/pages/Detect/Ex1.js
@@ -5,7 +5,7 @@ function Ex1() {
     return (
         <>
             <h3>Discover Data Exfiltration 1</h3>
-            <p style={{ fontSize: '12px' }}>Discover Data Exfiltration 1 refers to a type of security incident where sensitive data is deliberately removed from an organization's network. This can be done by an insider, such as an employee or contractor, or by an external attacker who has gained unauthorized access to the network. The purpose of data exfiltration is typically to steal sensitive information or to compromise the security of the target organization. "Discover Data Exfiltration 1" could be the name of a specific tool, product, or service that helps organizations detect and prevent data exfiltration incidents.</p>
+            <p style={{ fontSize: '12px' }}>Discover Data Exfiltration 1 refers to a type of security incident where sensitive data is deliberately removed from an organization's network. This can be done by an insider, such as an employee or contractor, or by an external attacker who has gained unauthorized access to the network. The purpose of data exfiltration is typically to steal sensitive information or to compromise the security of the target organization.</p>
             <UploadImg />
 
 
