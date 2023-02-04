95fc48ff4113d1ed209a861f67efe0530839227c
harsh0989 <harshmangukiya1506@gmail.com>
2023-02-05 05:01:48 +0530

    Updated Ex3.js

--- src/pages/Detect/Ex3.js
+++ src/pages/Detect/Ex3.js
@@ -4,7 +4,7 @@ import UploadImg from '../../sections/@dashboard/barcode/UploadImg'
 function Ex3() {
     return (
         <>
-            <h3>Discover Data Exfiltration 1</h3>
+            <h3>Discover Data Exfiltration 3</h3>
             <p style={{ fontSize: '12px' }}>Discover Data Exfiltration 3 refers to some assailant grabbing all the employee and vendor email addresses, and sending them out on a channel normally reserved for other uses. This is similar to attackers abusing DNS for data exfiltration. One host is sending out much more data on a some port from the enterprise than other hosts do, different from the hosts in 1 and 2.</p>
             <UploadImg />
 
