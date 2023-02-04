689f56b5833f416a6b2cfaa132c1b6d7b29b7bf5
harsh0989 <harshmangukiya1506@gmail.com>
2023-02-05 05:00:22 +0530

    LateralSpy.js created

---
+++ src/pages/Detect/LateralSpy.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import UploadImg from '../../sections/@dashboard/barcode/UploadImg'
+
+function LateralSpy() {
+    return (
+        <>
+            <h3>Lateral Spy</h3>
+            <p style={{ fontSize: '12px' }}>Lateral spying refers to the unauthorized access or theft of confidential or sensitive information within an organization by an insider, such as an employee, contractor, or partner. This type of espionage is often more damaging than attacks from external sources because insiders have legitimate access to the organization's networks and systems, making it easier for them to steal or compromise sensitive information.</p>
+            <UploadImg />
+
+
+        </>
+    )
+}
+
+export default LateralSpy
