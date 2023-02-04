59dc2a72c822346638065769597195bbf62c70f4
harsh0989 <harshmangukiya1506@gmail.com>
2023-02-05 04:57:38 +0530

    botnet.js created

---
+++ src/pages/Detect/Botnet.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import UploadImg from '../../sections/@dashboard/barcode/UploadImg'
+
+function Botnet() {
+    return (
+        <>
+            <h3>Botnet</h3>
+            <p style={{ fontSize: '12px' }}>A botnet is a network of compromised computers, also known as "bots," that are controlled remotely by a single attacker or a group of attackers. Botnets can be used for a variety of malicious purposes, such as sending spam messages, launching distributed denial of service (DDoS) attacks, stealing sensitive information, and spreading malware.</p>
+            <UploadImg />
+
+
+        </>
+    )
+}
+
+export default Botnet
